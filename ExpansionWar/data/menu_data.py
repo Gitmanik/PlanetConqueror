70ae7db5043f4aba73b2f211452c35fd7e55cff9
Paweł Reich <37516105+Gitmanik@users.noreply.github.com>
2025-04-08 10:23:53 +0200

    Added CLIENT and HOST buttons to GameConfigScene + autosave config

--- ExpansionWar/data/menu_data.py
+++ ExpansionWar/data/menu_data.py
@@ -6,23 +6,26 @@ from managers.save_manager import SaveManager
 logger = logging.getLogger(__name__)
 
 class MenuData:
-    def __init__(self, mode : str, ip : str, port : str):
+    def __init__(self, mode : str, ip : str, port : str, network_mode : str):
         self.mode = mode
         self.ip = ip
         self.port = port
+        self.network_mode = network_mode
 
     def to_dict(self):
         return {
             'mode': self.mode,
             'ip': self.ip,
             'port': self.port,
+            'network_mode': self.network_mode,
         }
 
     @staticmethod
     def from_dict(data):
         return MenuData(data['mode'],
                         data['ip'],
-                        data['port'])
+                        data['port'],
+                        data['network_mode'])
 
     # ── JSON Support ──
 
